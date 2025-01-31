a5fa32e7988ba4a4663e2e4ec797997ebb0ab459
onozawakazutoshi <146906422+onozawakazutoshi@users.noreply.github.com>
2025-01-31 11:18:27 +0900

    //完成

--- main.cpp
+++ main.cpp
@@ -1,8 +1,23 @@
-#include "Enemy.h"
+template <typename T1,typename T2>
+#include <stdio.h>
+class Box{
+public:
+	T1 x;
+	T2 y;
+	Box(T1 x, T2 y) :x(x), y(y){}
+
+	T1 Min() {
+		if (x < y) {
+			return x;
+		}
+		else {
+			return y;
+		}
+	}
+
+};
 int main() {
-	Enemy* enemy = new Enemy;
-	enemy->Move();
-	enemy->Atack();
-	enemy->Remove();
+	Box<int, int> b1(10, 20);
+	printf("Min:%d", b1.Min());
 	return 0;
 }
